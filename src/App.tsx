724292a60654bd23a2f88404b08ff2406d052a58
obov <obov3e@gmail.com>
2023-01-24 17:08:55 +0900

    tailwind

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="bg-red-200">hi</div>;
+}
+
+export default App;
